b734c52c116038514275f16305d500716a59a38d
Hellicat <jhccop@naver.com>
2023-10-16 12:06:11 +0900

    edit style & gulp

---
+++ README.md
@@ -0,0 +1,26 @@
+# Clone - z-o-o.fr
+
+Clone Coding !!
+https://www.z-o-o.fr/
+
+Developed by HTML, SCSS
+Built By Gulp
+Use Live Server (VScode Extention)
+
+#### Develope
+
+```
+npm run dev
+```
+
+#### Build
+
+```
+npm run build
+```
+
+#### Deploy
+
+```
+npm run deploy
+```
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,19 +1,40 @@
 import gulp from "gulp";
-import { deleteSync } from "del";
+import {deleteSync} from "del";
 import minify from "gulp-csso";
 import gulpSass from "gulp-sass";
 import sass2 from "sass";
 import autoprefixer from "gulp-autoprefixer";
+import replace from "gulp-replace";
+import htmlmin from "gulp-htmlmin";
+import imagemin from "gulp-imagemin";
+import ghPages from "gh-pages";
 
 const sass = gulpSass(sass2);
 
 const routes = {
+  html: {
+    src: "index.html",
+    dest: "dest",
+  },
   css: {
     watch: "src/scss/*",
     src: "src/scss/styles.scss",
     dest: "dest/css",
   },
+  img: {
+    watch: "src/img/**/*",
+    src: "src/img/**/*",
+    dest: "dest/img",
+  },
 };
+
+const homepage = () =>
+  gulp
+    .src(routes.html.src)
+    .pipe(replace("dest/css/", "css/"))
+    .pipe(replace("src/img/", "img/"))
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(routes.html.dest));
 
 const styles = () =>
   gulp
@@ -28,16 +49,26 @@ const styles = () =>
     .pipe(minify())
     .pipe(gulp.dest(routes.css.dest));
 
+const image = () =>
+  gulp.src(routes.img.src).pipe(imagemin()).pipe(gulp.dest(routes.img.dest));
+
 const watch = () => {
   gulp.watch(routes.css.watch, styles);
 };
+const ghDeploy = () =>
+  ghPages.publish("dest", {
+    branch: "gh-pages",
+    message: "Auto Deploy",
+  });
 
 const clean = async () => await deleteSync(["dest/"]);
 
 const prepare = gulp.series([clean]);
 
-const assets = gulp.series([styles]);
+const assets = gulp.series([homepage, styles, image]);
 
 const live = gulp.parallel([watch]);
 
-export const dev = gulp.series([prepare, assets, live]);
+export const build = gulp.series([prepare, assets]);
+export const dev = gulp.series([build, live]);
+export const deploy = gulp.series([build, ghDeploy, prepare]);
